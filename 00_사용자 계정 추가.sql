3402c8c7c04b079d310d7aafe21f3e5145e5ea4a
use-da <110652066+use-da@users.noreply.github.com>
2022-09-01 09:16:36 +0900

    Initial commit

---
+++ 00_사용자 계정 추가.sql
@@ -0,0 +1,24 @@
+--한 줄 주석
+
+/*
+ * 범위 주석
+ * */
+
+--SQL 하나 수행 : CTRL+ENTER
+ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
+
+--실습용 사용자 계정 생성
+CREATE USER lyh IDENTIFIED BY kh1234;
+
+--사용자 계정 권한 부여
+GRANT RESOURCE, CONNECT TO lyh;
+
+--객체 생성(테이블 등) 공간 할당량 지정
+ALTER USER lyh DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+
+
+
+
+
+
+
